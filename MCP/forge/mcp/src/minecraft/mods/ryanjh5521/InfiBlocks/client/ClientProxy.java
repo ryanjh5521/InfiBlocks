df5fa185cc542c8b2da4036fb367be3c10213b74
Ryanjh5521 <ryanjh5521@gmail.com>
2013-06-15 14:59:35 -0400

    Just messing around

---
+++ MCP/forge/mcp/src/minecraft/mods/ryanjh5521/InfiBlocks/client/ClientProxy.java
@@ -0,0 +1,12 @@
+package mods.ryanjh5521.InfiBlocks.client;
+
+import net.minecraftforge.client.MinecraftForgeClient;
+import mods.ryanjh5521.InfiBlocks.common.CommonProxy;
+
+public class ClientProxy extends CommonProxy {
+        
+        @Override
+        public void registerRenderers() {
+        }
+        
+}
