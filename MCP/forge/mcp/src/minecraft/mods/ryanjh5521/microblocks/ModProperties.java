fb84f6163cab898e616564209eb50a949da4d648
Ryanjh5521 <ryanjh5521@gmail.com>
2013-06-15 23:20:33 -0400

    Update to MANIFEST. MicroBlocks now works fully

--- MCP/forge/mcp/src/minecraft/mods/ryanjh5521/microblocks/ModProperties.java
+++ MCP/forge/mcp/src/minecraft/mods/ryanjh5521/microblocks/ModProperties.java
@@ -2,6 +2,6 @@ package mods.ryanjh5521.microblocks;
 
 public class ModProperties {
 	public static final String MOD_VERSION = "55.0.1";
-	public static final String MODID = "ryanjh5521Microblocks";
-	public static final String MOD_NAME = "ryanjh5521's Microblocks";
+	public static final String MODID = "InfiMicroblocks";
+	public static final String MOD_NAME = "InfiBlocks-Microblocks";
 }
