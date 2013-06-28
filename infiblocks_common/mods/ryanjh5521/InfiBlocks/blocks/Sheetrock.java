eb183330842e83899233d797ccfd6c74f83eb2a4
Ryanjh5521 <ryanjh5521@gmail.com>
2013-06-27 21:09:28 -0400

    Testing some Sheetrocks

--- infiblocks_common/mods/ryanjh5521/InfiBlocks/blocks/Sheetrock.java
+++ infiblocks_common/mods/ryanjh5521/InfiBlocks/blocks/Sheetrock.java
@@ -1,8 +1,5 @@
 package mods.ryanjh5521.InfiBlocks.blocks;
 
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
-
 import java.util.List;
 
 import net.minecraft.block.Block;
@@ -14,75 +11,47 @@ import net.minecraft.util.Icon;
 
 public class Sheetrock extends Block
 {
-    @SideOnly(Side.CLIENT)
-    private Icon[] iconArray;
+	private static final String[] Types = new String[] {"sheetrock_blue", "sheetrock_purple", "sheetrock_cyan", "sheetrock_yellow", "sheetrock_green", "sheetrock_orange", "sheetrock_white", "sheetrock_red"};
+	private Icon[] textures;
 
-    public Sheetrock(int par1)
-    {
-        super(par1, Material.cloth);
-        this.setCreativeTab(CreativeTabs.tabBlock);
-    }
+	public Sheetrock(int blockID)
+	{
+		super(blockID, Material.cloth);
+		setHardness(1.0F);
+		this.setCreativeTab(CreativeTabs.tabBlock);
+	}
 
-    @SideOnly(Side.CLIENT)
+	@Override
+	public void registerIcons(IconRegister iconRegister)
+	{
+		textures = new Icon[Types.length];
 
-    /**
-     * From the specified side and block metadata retrieves the blocks texture. Args: side, metadata
-     */
-    public Icon getIcon(int par1, int par2)
-    {
-        return this.iconArray[par2 % this.iconArray.length];
-    }
+		for (int i = 0; i < Types.length; ++i) {
+			textures[i] = iconRegister.registerIcon("InfiBlocks:"+Types[i]);
+		}
+	}
 
-    /**
-     * Determines the damage on the item the block drops. Used in cloth and wood.
-     */
-    public int damageDropped(int par1)
-    {
-        return par1;
-    }
+	@Override
+	public Icon getIcon(int side, int meta)
+	{
+		if (meta < 0 || meta >= textures.length) {
+			meta = 0;
+		}
 
-    /**
-     * Takes a dye damage value and returns the block damage value to match
-     */
-    public static int getBlockFromDye(int par0)
-    {
-        return ~par0 & 15;
-    }
+		return textures[meta];
+	}
 
-    /**
-     * Takes a block damage value and returns the dye damage value to match
-     */
-    public static int getDyeFromBlock(int par0)
-    {
-        return ~par0 & 15;
-    }
+	@Override
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public void getSubBlocks(int blockID, CreativeTabs creativeTabs, List list) {
+		for (int i = 0; i < Types.length; ++i) {
+			list.add(new ItemStack(blockID, 1, i));
+		}
+	}
 
-    @SideOnly(Side.CLIENT)
-
-    /**
-     * returns a list of blocks with the same ID, but different meta (eg: wood returns 4 blocks)
-     */
-    public void getSubBlocks(int par1, CreativeTabs par2CreativeTabs, List par3List)
-    {
-        for (int j = 0; j < 16; ++j)
-        {
-            par3List.add(new ItemStack(par1, 1, j));
-        }
-    }
-
-    @SideOnly(Side.CLIENT)
-
-    /**
-     * When this method is called, your block should register all the icons it needs with the given IconRegister. This
-     * is the only chance you get to register icons.
-     */
-    public void registerIcons(IconRegister par1IconRegister)
-    {
-        this.iconArray = new Icon[16];
-
-        for (int i = 0; i < this.iconArray.length; ++i)
-        {
-            this.iconArray[i] = par1IconRegister.registerIcon("InfiBlocks:Sheetrock" + i);
-        }
-    }
+	@Override
+	public int damageDropped(int meta)
+	{
+		return meta;
+	}
 }
